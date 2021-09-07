523e19b75f735a955643ca5bc52904be2640e920
Crazy-Newbie <ezie2001@gmail.com>
2021-09-07 15:03:36 +0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Poppins']
+      },
+      colors: {
+        primary: '#1b1b1b',
+        secondary: '#F4F4F4',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
